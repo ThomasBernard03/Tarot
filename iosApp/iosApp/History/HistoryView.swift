be5552e2654456f341cf4200c33ec4be99d4a0b7
tbernard@pageup.fr <thomas.bernard@ikmail.com>
2024-07-21 15:57:49 +0200

    Can delete a game

--- iosApp/iosApp/History/HistoryView.swift
+++ iosApp/iosApp/History/HistoryView.swift
@@ -11,6 +11,8 @@ import Shared
 
 struct HistoryView: View {
     private let getGameHistoryUseCase = GetGameHistoryUseCase()
+    private let deleteGameUseCase = DeleteGameUseCase()
+    
     @State private var games: [GameModel] = []
     
     var body: some View {
@@ -20,12 +22,44 @@ struct HistoryView: View {
                     Text("Aucune partie réalisée")
                 }
                 else {
-                    List(games, id: \.id) { game in
-                        Text(game.startedAt.time.description())
+                    List {
+                        ForEach(games, id: \.id){ game in
+                            NavigationLink(destination: { EmptyView() }) {
+                                HStack(spacing: 2) {
+                                    ForEach(game.players, id : \.id){ player in
+                                        ZStack {
+                                            Circle()
+                                                .foregroundColor(player.color.toColor())
+                                                .frame(width: 25, height: 25)
+                                            Text("" + player.name.uppercased().prefix(1))
+                                                .foregroundColor(.white)
+                                                .font(.system(size: 12))
+                                        }
+                                    }
+                                    
+                                    Spacer()
+                                    
+                                    // Text(date.timeIntervalSinceNow)
+                                    
+                                }
+                            }
+                        }
+                        .onDelete { index in
+                            let game = games[index.first!]
+                            
+                            deleteGameUseCase.invoke(id: game.id) { result, _ in
+                                if result?.isSuccess() ?? false {
+                                    games.remove(atOffsets: index)
+                                }
+                            }
+                        }
                     }
                 }
             }
             .navigationTitle("Historique")
+            .toolbar {
+                EditButton()
+            }
             .onAppear {
                 getGameHistoryUseCase.invoke { result, _ in
                     if result?.isSuccess() ?? false {
